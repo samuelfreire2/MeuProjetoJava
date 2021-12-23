8abcec380c469540d4294e2bb740408d61ff406f
samuel freire <samuellfreire@gmail.com>
2021-12-23 09:33:39 -0300

    Criaçaão do Projeto

---
+++ Meu Primeiro Programa Java/src/CursoJavaExecutavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package CursoJavaExecutavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um metodo executavel em Java*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Meu primeiro programa em java");
+		
+		
+	}
+
+}
