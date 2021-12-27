2cd2759fe8506ff78edc36158a07c348da10884b
samuel freire <samuellfreire@gmail.com>
2021-12-27 13:48:00 -0300

    Commit Nota Aluno

--- Meu Primeiro Programa Java/src/CursoJavaExecutavel/PrimeiraClasseJava.java
+++ Meu Primeiro Programa Java/src/CursoJavaExecutavel/PrimeiraClasseJava.java
@@ -1,17 +1,43 @@
 package CursoJavaExecutavel;
 
+import javax.swing.JOptionPane;
+
 public class PrimeiraClasseJava {
-	
-	/*Main é um metodo executavel em Java*/
+
+	/* Main é um metodo executavel em Java */
 	public static void main(String[] args) {
 		
-		/*Escreve texto no console*/
-		System.out.println("Meu primeiro programa em java");
+       
+	String nota1 = JOptionPane.showInputDialog("Informe a mota 1 ");
+	String nota2 = JOptionPane.showInputDialog("Informe a mota 2 ");
+	String nota3 = JOptionPane.showInputDialog("Informe a mota 3 ");
+	String nota4 = JOptionPane.showInputDialog("Informe a mota 4 ");
+	
+	
+	double dNota1 = Double.parseDouble(nota1);
+	double dNota2 = Double.parseDouble(nota2);
+	double dNota3 = Double.parseDouble(nota3);
+	double dNota4 = Double.parseDouble(nota4);
+	
+	double media = (dNota1 + dNota2 + dNota3 + dNota4) / 4;
+	
+	/* Média para aprovação é 70*/
+	
+	if (media >= 50) {
+	
+	if (media >= 70) {
+		JOptionPane.showConfirmDialog(null, "Aluno está aprovado com média de :" + media);
+	}else {
+		JOptionPane.showConfirmDialog(null, "Aluno em recuperação com média de :" + media);
 		
-		
-		boolean forsa;
-		
-		
+	
+	JOptionPane.showConfirmDialog(null, "Aluno está reprovado com média de  :" + media);
+	
+			
+		}
+			
+     }
+     
 	}
 
 }
