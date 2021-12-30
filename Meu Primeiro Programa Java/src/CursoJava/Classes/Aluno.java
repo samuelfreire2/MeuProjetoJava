902f7ec375d16b8ac92f24eb3bb6c53a9932938a
samuel freire <samuellfreire@gmail.com>
2021-12-30 08:08:20 -0300

    Commit Disciplina

--- Meu Primeiro Programa Java/src/CursoJava/Classes/Aluno.java
+++ Meu Primeiro Programa Java/src/CursoJava/Classes/Aluno.java
@@ -15,14 +15,18 @@ public class Aluno {
 	private String nomeEscola;
 	private String serieMatriculado;
 	
-	private double nota1;
-	private double nota2;
-	private double nota3;
-	private double nota4;
+	private Disciplina disciplina = new Disciplina();
 	
 	
+	public void setDisciplina(Disciplina disciplina) {
+		this.disciplina = disciplina;
+	}
 	
+	public Disciplina getDisciplina() {
+		return disciplina;
+	}
 	
+
 	public Aluno() {/*Cria os dados na memoria - Sendo padrão do java*/
 		
 	}
@@ -121,40 +125,10 @@ public class Aluno {
 		this.serieMatriculado = serieMatriculado;
 	}
 
-	public double getNota1() {
-		return nota1;
-	}
-
-	public void setNota1(double nota1) {
-		this.nota1 = nota1;
-	}
-
-	public double getNota2() {
-		return nota2;
-	}
-
-	public void setNota2(double nota2) {
-		this.nota2 = nota2;
-	}
-
-	public double getNota3() {
-		return nota3;
-	}
-
-	public void setNota3(double nota3) {
-		this.nota3 = nota3;
-	}
-
-	public double getNota4() {
-		return nota4;
-	}
-
-	public void setNota4(double nota4) {
-		this.nota4 = nota4;
-	}
 	/*Método que retorna a média do aluno*/
 	public double getMediaNota() {
-		return (nota1 + nota2 + nota3 + nota4) / 4;
+		return (disciplina.getNota1() + disciplina.getNota2() 
+				+ disciplina.getNota3() + disciplina.getNota4()) / 4;
 	}
 	/*Métudo que retorna true para aprovado e false para reprovado*/
     public boolean getAlunoAprovado() {
@@ -173,7 +147,107 @@ public class Aluno {
 		}else {
 			return "Aluno está reprovado";
 		}
-    }	
+    }
+
+	@Override
+	public String toString() {
+		return "Aluno [nome=" + nome + ", idade=" + idade + ", dataNascimento=" + dataNascimento + ", registroGeral="
+				+ registroGeral + ", numeroCpf=" + numeroCpf + ", nomeMae=" + nomeMae + ", nomePai=" + nomePai
+				+ ", dataMatricula=" + dataMatricula + ", nomeEscola=" + nomeEscola + ", serieMatriculado="
+				+ serieMatriculado + ", disciplina=" + disciplina + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dataMatricula == null) ? 0 : dataMatricula.hashCode());
+		result = prime * result + ((dataNascimento == null) ? 0 : dataNascimento.hashCode());
+		result = prime * result + ((disciplina == null) ? 0 : disciplina.hashCode());
+		result = prime * result + idade;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((nomeEscola == null) ? 0 : nomeEscola.hashCode());
+		result = prime * result + ((nomeMae == null) ? 0 : nomeMae.hashCode());
+		result = prime * result + ((nomePai == null) ? 0 : nomePai.hashCode());
+		result = prime * result + ((numeroCpf == null) ? 0 : numeroCpf.hashCode());
+		result = prime * result + ((registroGeral == null) ? 0 : registroGeral.hashCode());
+		result = prime * result + ((serieMatriculado == null) ? 0 : serieMatriculado.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (dataMatricula == null) {
+			if (other.dataMatricula != null)
+				return false;
+		} else if (!dataMatricula.equals(other.dataMatricula))
+			return false;
+		if (dataNascimento == null) {
+			if (other.dataNascimento != null)
+				return false;
+		} else if (!dataNascimento.equals(other.dataNascimento))
+			return false;
+		if (disciplina == null) {
+			if (other.disciplina != null)
+				return false;
+		} else if (!disciplina.equals(other.disciplina))
+			return false;
+		if (idade != other.idade)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (nomeEscola == null) {
+			if (other.nomeEscola != null)
+				return false;
+		} else if (!nomeEscola.equals(other.nomeEscola))
+			return false;
+		if (nomeMae == null) {
+			if (other.nomeMae != null)
+				return false;
+		} else if (!nomeMae.equals(other.nomeMae))
+			return false;
+		if (nomePai == null) {
+			if (other.nomePai != null)
+				return false;
+		} else if (!nomePai.equals(other.nomePai))
+			return false;
+		if (numeroCpf == null) {
+			if (other.numeroCpf != null)
+				return false;
+		} else if (!numeroCpf.equals(other.numeroCpf))
+			return false;
+		if (registroGeral == null) {
+			if (other.registroGeral != null)
+				return false;
+		} else if (!registroGeral.equals(other.registroGeral))
+			return false;
+		if (serieMatriculado == null) {
+			if (other.serieMatriculado != null)
+				return false;
+		} else if (!serieMatriculado.equals(other.serieMatriculado))
+			return false;
+		return true;
+	}	
     
 }
 	
+
+
+
+
+
+
+
+
+
+
--- Meu Primeiro Programa Java/src/CursoJavaExecutavel/PrimeiraClasseJava.java
+++ Meu Primeiro Programa Java/src/CursoJavaExecutavel/PrimeiraClasseJava.java
@@ -23,9 +23,17 @@ public class PrimeiraClasseJava {
 		String matricula = JOptionPane.showInputDialog("Qual é a Matricula?");
 		String escola = JOptionPane.showInputDialog("Nome Escola?");
 		String serie = JOptionPane.showInputDialog("Qual a série");
+		
+		String disciplina1 = JOptionPane.showInputDialog("Disciplina 1?");
 		String nota1 = JOptionPane.showInputDialog("Nota 1");
+		
+		String disciplina2 = JOptionPane.showInputDialog("Disciplina 2?");
 		String nota2 = JOptionPane.showInputDialog("Nota 2");
+		
+		String disciplina3 = JOptionPane.showInputDialog("Disciplina 3?");
 		String nota3 = JOptionPane.showInputDialog("Nota 3");
+		
+		String disciplina4 = JOptionPane.showInputDialog("Disciplina 4?");
 		String nota4 = JOptionPane.showInputDialog("Nota 4");
 		
 		Aluno aluno1 = new Aluno();/*Aqui será o joão*/
@@ -40,25 +48,21 @@ public class PrimeiraClasseJava {
 		aluno1.setDataMatricula(matricula);
 		aluno1.setNomeEscola(escola);
 		aluno1.setSerieMatriculado(serie);
-		aluno1.setNota1(Double.parseDouble(nota1));
-		aluno1.setNota2(Double.parseDouble(nota2));
-		aluno1.setNota3(Double.parseDouble(nota3));
-		aluno1.setNota4(Double.parseDouble(nota4));
 		
-		System.out.println("Nome é = " + aluno1.getNome());
-		System.out.println("Idade é = " + aluno1.getIdade());
-		System.out.println("Nascimento = " + aluno1.getDataNascimento());
-		System.out.println("Rg = " + aluno1.getRegistroGeral());
-		System.out.println("Cpf = " + aluno1.getNumeroCpf());
-		System.out.println("Nome mãe = " + aluno1.getNomeMae());
-		System.out.println("Nome pai = " + aluno1.getNomePai());
-		System.out.println("Matricula = " + aluno1.getDataMatricula());
-		System.out.println("Nome Escola = " + aluno1.getNomeEscola());
-		System.out.println("Série Matriculado = " + aluno1.getSerieMatriculado());
-		System.out.println("Média da nota é = " + aluno1.getMediaNota());
-		System.out.println("Resultado = " + (aluno1.getAlunoAprovado() ? "Aprovado" : "Reprovado"));
-		System.out.println("Resultado 2 = " + aluno1.getAlunoAprovado2()); 
-        
+		
+		aluno1.getDisciplina().setNota1(Double.parseDouble(nota1));
+		aluno1.getDisciplina().setNota2(Double.parseDouble(nota2));
+		aluno1.getDisciplina().setNota3(Double.parseDouble(nota3));
+		aluno1.getDisciplina().setNota4(Double.parseDouble(nota4));
+		
+		aluno1.getDisciplina().setDisciplina1(disciplina1);
+		aluno1.getDisciplina().setDisciplina2(disciplina2);
+		aluno1.getDisciplina().setDisciplina3(disciplina3);
+		aluno1.getDisciplina().setDisciplina4(disciplina4);
+		
+		System.out.println(aluno1); /*Descrição do objeto na memoria*/
+		System.out.println("Média do aluno = " + aluno1.getMediaNota());
+		System.out.println("Resultado = " + aluno1.getAlunoAprovado2());
 		
 		/*========================================================================*/
 		System.out.println("-----------------------------------------------------------");
